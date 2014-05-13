568ac1bc430dfbbb91fdee34a20b0013a0fd6ce2
zcb <zcb0821@163.com>
2014-05-13 22:40:30 +0800

    增加了model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+# Create your product model here.
+class Product(models):
+    pass
+
+
+# Create your event model here.
+class Event(models):
+    pass
+
+
+# Create your administrator model here.
+class Administrator(models):
+    pass
