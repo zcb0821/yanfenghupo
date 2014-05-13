af132f67639af2a01bf294645a013e2c883f9698
duanruiqi <drq11@mails.tsinghua.edu.cn>
2014-05-13 22:35:12 +0800

    尝试建立数据库但连接失败 报错ImportError: no module named yanfeng 修改了model和setting

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    DisplayName = models.CharField(max_length=50)	#显示在图片目录页的名称
+    DetailName = models.CharField(max_length=100)	#显示在图片详情页的名称
+    material = models.CharField(max_length=50)	    #材质
+    series = models.CharField(max_length=50)	    #系列
+    function = models.CharField(max_length=50)	    #功能
+    origin =  models.CharField(max_length=50)	    #产地
--- yanfenghupo/settings.py
+++ yanfenghupo/settings.py
@@ -30,18 +30,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
+#    'django.contrib.auth',
+#    'django.contrib.contenttypes',
+#    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'yanfeng',
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
+#    'django.contrib.sessions.middleware.SessionMiddleware',
+#    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
+#    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
@@ -56,8 +57,12 @@ WSGI_APPLICATION = 'yanfenghupo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'yanfeng',
+        'USER': 'root',
+        'PASSWORD': '1111',
+        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '3306',                      # Set to empty string for default.
     }
 }
 
